dabc89a5111d0ca4e007a4866750577ccae1dd98
Jonattan Nieto Sánchez <jonattan.nieto.sanchez@gmail.com>
2017-05-18 15:26:11 +0200

    T21Cells Templates

---
+++ T21/T21Cells/ViewModelCollectionCell.xctemplate/___FILEBASENAME___CollectionCell.swift
@@ -0,0 +1,28 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//
+
+import UIKit
+
+class ___FILEBASENAME___CollectionCell: UICollectionViewCell {
+    
+    var viewModel = ___FILEBASENAME___CollectionCellViewModel
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
